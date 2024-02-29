57adccae7e40a801bae23e134080f3d60096b8d9
Ali <ali.chouchene@insat.univ-carthage.tn>
2024-02-29 22:25:24 +0100

    QMI8658C : add examples

---
+++ examples/AccelerometerOnly/AccelerometerOnly.ino
@@ -0,0 +1,44 @@
+#include "Qmi8658c.h"                     // Include the library for QMI8658C sensor
+
+#define QMI_ADDRESS 0x6B                  // Define QMI8658C I2C address
+#define QMI8658C_I2C_FREQUENCY 80000      // Define I2C frequency as 80kHz (in Hz)
+
+// Declare an instance of Qmi8658c
+Qmi8658c qmi8658c(QMI_ADDRESS, QMI8658C_I2C_FREQUENCY);
+
+/* QMI8658C configuration */
+qmi8658_cfg_t qmi8658_cfg = {
+  .qmi8658_mode = qmi8658_mode_acc_only, // Set the QMI8658C mode to accelerometer-only mode
+  .acc_scale = acc_scale_2g,             // Set the accelerometer scale to ±2g
+  .acc_odr = acc_odr_8000,               // Set the accelerometer output data rate (ODR) to 8000Hz
+};
+
+qmi8658_result_t qmi8658_result;
+
+void setup() {
+  // Setup serial communication
+  Serial.begin(9600);
+
+  // Initialize QMI8658C sensor with provided configuration
+  qmi8658_result = qmi8658c.open(&qmi8658_cfg);
+  delay(1000); // Delay for sensor initialization
+  Serial.print("Open result: ");
+  Serial.println(qmi8658c.resultToString(qmi8658_result)); // Print initialization result
+  Serial.print("Device ID: 0x");
+  Serial.println(qmi8658c.deviceID, HEX); // Print device ID
+  Serial.print("Device Revision ID: 0x");
+  Serial.println(qmi8658c.deviceRevisionID, HEX); // Print device revision ID
+}
+
+qmi_data_t data; // Declare a variable to store sensor data
+
+void loop() {
+  qmi8658c.read(&data); // Read accelerometer data from QMI8658C sensor
+  Serial.print("X: ");
+  Serial.print(data.acc_xyz.x);
+  Serial.print(" | Y: ");
+  Serial.print(data.acc_xyz.y);
+  Serial.print(" | Z: ");
+  Serial.println(data.acc_xyz.z);
+  delay(100);
+}
